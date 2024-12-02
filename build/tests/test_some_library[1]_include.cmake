3e42810b206c1491b5b8aa8d37a15622a9720986
Angel-lope <angellopez6193@gmail.com>
2024-12-02 09:42:58 +0100

    Delete build directory

--- build/tests/test_some_library[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/angel/project/build/tests/test_some_library[1]_tests.cmake")
-  include("/home/angel/project/build/tests/test_some_library[1]_tests.cmake")
-else()
-  add_test(test_some_library_NOT_BUILT test_some_library_NOT_BUILT)
-endif()
